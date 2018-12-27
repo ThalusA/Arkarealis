9f82b94c7a5a58a558958bce0136c6d1b4c7f089
ThalusA <gamingonline69@orange.fr>
2018-12-27 22:36:39 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,14 +1,15 @@
 @echo off
 color 07
-IF EXIST build (
-    set MainPath = dir /s /b main.exe
-    "%MainPath%"
-) ELSE (
-    choice /M "Project not build, do you want to build it then launch it ? "
-    IF ERRORLEVEL 1 GOTO BUILDING
-    IF ERRORLEVEL 2 GOTO EXIT 
-)
+:START
+dir /s /b main.exe>tmpFile
+set /p MainPath=<tmpFile
+del tmpFile
+IF /I "%MainPath%" NEQ "" CALL "%MainPath%" && GOTO EXIT
+choice /M "Project not build, do you want to build it then launch it ? "
+IF ERRORLEVEL==1 GOTO BUILDING
+IF ERRORLEVEL==2 GOTO EXIT 
 :BUILDING
-start setup.bat
-:EXIT 
+setup.bat
+GOTO START
+:EXIT
 pause
